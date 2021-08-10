6997959e4d8402f72421b62634a57ab9b90c4021
Jason Lee <jason@steeplesoft.com>
2021-08-10 10:12:31 -0500

    Project structure cleanup
    Remove dependency module, as it's not needed
    Update build and tests to reflect module removal

--- example/src/main/java/org/wildfly/extension/feature/pack/template/example/JaxRsResource.java
+++ example/src/main/java/org/wildfly/extension/feature/pack/template/example/JaxRsResource.java
@@ -1,12 +1,8 @@
 package org.wildfly.extension.feature.pack.template.example;
 
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-
-import org.wildfly.feature.pack.template.dependency.ExampleQualifier;
-import org.wildfly.feature.pack.template.dependency.Message;
 
 /**
  * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
@@ -15,14 +11,10 @@ import org.wildfly.feature.pack.template.dependency.Message;
 @Path("/")
 public class JaxRsResource {
 
-    @Inject
-    @ExampleQualifier
-    Message greeting;
-
 
     @GET
     @Path("/greeting")
     public String getGreeting() {
-        return greeting.getMessage() + "!";
+        return "!";
     }
 }
